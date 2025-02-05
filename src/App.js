506fc2ac3511cb34901881e76f1949d712c50b6a
vishwas <vishwasmyya19@gmail.com>
2025-02-06 00:48:42 +0530

    changed name of file

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <ScrollToTop />
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/home" exact component={Home} />
         <Route path="/services" component={Services} />
         <Route path="/products" component={Products} />
 
