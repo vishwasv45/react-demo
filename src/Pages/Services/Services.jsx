37bf7370b411e01a9aa99bf61f24065e424269c0
vishwas <vishwasmyya19@gmail.com>
2025-02-06 15:47:51 +0530

    added datalayer

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -3,6 +3,17 @@ import InfoSection from "../../compoents/InfoSection/InfoSection";
 import Pricing from "../../compoents/Pricing/Pricing";
 import { homeObjOne1, homeObjOne2, homeObjOne3, homeObjOne4 } from "../Data";
 
+window.dataLayer={
+     event: 'pageView',
+        pageView: viewName,
+        userId: '12345',
+        userType: 'guest',
+        pageCategory: 'home',
+        pageTitle: document.title,
+        timestamp: new Date().toString(),
+        pageUrl: window.location.href,
+}
+
 function Services() {
   return (
     <>
