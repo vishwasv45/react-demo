8689e843f6e7c1702b6ae3d6ddae994a8e6f17df
vishwas <vishwasmyya19@gmail.com>
2025-02-06 12:09:28 +0530

    changed the home page

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,6 +1,25 @@
-window.pageDataLayer={
-  'event': 'pageView',
-  'viewName': "home"
+function onViewChange() {
+
+  var viewName = window.location.hash; // or use window.location.pathName if router works on path and not hash
+
+  viewName = viewName || 'home'; // view name cannot be empty
+
+  // Sanitize viewName to get rid of any trailing symbols derived from URL
+
+  if (viewName.startsWith('#') || viewName.startsWith('/')) {
+    viewName = viewName.substr(1);
+  }
+
+  alloy("sendEvent", {
+    "renderDecisions": true,
+    "xdm": {
+      "web": {
+        "webPageDetails": {
+          "viewName":"home"
+        }
+      }
+    }
+  });
 }
 
 
@@ -9,6 +28,7 @@ import React from 'react'
 import InfoSection from "../../compoents/InfoSection/InfoSection"
 import Pricing from '../../compoents/Pricing/Pricing';
 import { homeObjOne1, homeObjOne2, homeObjOne3, homeObjOne4 } from "../Data";
+
 const Home = () => {
   return (
     <>
