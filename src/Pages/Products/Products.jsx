679559e22d1fa455d027c1da952e784729de9450
vishwas <vishwasmyya19@gmail.com>
2025-02-06 16:02:24 +0530

    changed index

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import  InfoSection  from "../../compoents/InfoSection/InfoSection";
 import { homeObjOne1, homeObjOne2, homeObjOne3, homeObjOne4 } from "../Data";
-window.datalayer={
-  event: 'pageView',
-  pageView: viewName,
-  userId: '12345',
-  userType: 'guest',
-  pageCategory: 'home',
-  pageTitle: document.title,
-  timestamp: new Date().toString(),
-  pageUrl: window.location.href,
-}
+
 function Products() {
   return (
     <>
